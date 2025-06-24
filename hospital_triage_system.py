c5bd8109e95987aa03c7a24f34fe46912edf6564
Lucas <carneirolucas88@gmail.com>
2025-06-24 14:35:27 -0300

    feat: start implementation of linked list for patient triage

---
+++ hospital_triage_system.py
@@ -0,0 +1,16 @@
+# Exigencia de código 1 de 7
+# Implementar lista encadeada simples para pacientes
+
+# Cartão / Paciente
+class Patient:
+    def __init__(self, number, color):
+        self.number = number
+        self.color = color
+        self.next = None  # Referência para o próximo paciente na lista
+
+# Lista encadeada simples para pacientes
+class PatientList:
+    def __init__(self):
+        self.head = Node
+        self.number_card_green = 1
+        self.number_card_yellow = 201
