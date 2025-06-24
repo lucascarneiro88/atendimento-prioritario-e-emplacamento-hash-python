e232db01b444668dc91fc1b8cc702f586af3983d
Lucas <carneirolucas88@gmail.com>
2025-06-24 15:34:24 -0300

    feat: implement insertWithPriority method to add patients with priority

--- hospital_triage/hospital_triage_system.py
+++ hospital_triage/hospital_triage_system.py
@@ -17,7 +17,7 @@ class PatientList:
 
 
 # Exigência de código 2 de 7
-# Implementar método para adicionar pacientes sem prioridade à lista
+# Implementar função para add pacientes sem prioridade à lista
 # inserir no final da lista
 
     def insertWithoutPriority(self, patient):
@@ -28,6 +28,21 @@ class PatientList:
             while current.next:
                 current = current.next
             current.next = patient
+
+# Exigência de código 3 de 7
+# Implementar função para add pacientes com prioridade a lista
+# inserir após todos os nodos(pacientes) com prioridade( cartão amarelo - A ) mas sempre antes dos pacientes sem prioridade (cartão verde - V)
+
+def insertWithPriority(self, patient):
+    if not self.head or self.head.color == 'V':
+        patient.next = self.head
+        self.head = patient
+    else:
+        current = self.head
+        while current.next and current.next.color == 'A':
+            current = current.next
+        patient.next = current.next
+        current.next = patient
 
     
 
